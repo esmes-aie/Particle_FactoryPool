ac11e488d900b9a6f01c0f5c674554fb583ae1d7
esmes <esmes@SEA-167631>
2017-02-07 11:35:07 -0800

    particle image added

--- ParticleExample/main.cpp
+++ ParticleExample/main.cpp
@@ -3,11 +3,17 @@
 
 
 void main()
-{
+{ 
 	sfw::initContext();
 
 
-	while (sfw::stepContext());
+	unsigned sprite = 
+	sfw::loadTextureMap("../res/particle_sprite.png");
+
+	while (sfw::stepContext())
+	{
+		sfw::drawTexture(sprite, 400, 300, 100, 100);
+	}
 
 	sfw::termContext();
 
